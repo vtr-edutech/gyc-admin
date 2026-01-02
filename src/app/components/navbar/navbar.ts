a94b8fd1f4c192a612ea463a9e8b5871263ada32
Sachin / <sreesachin11226@gmail.com>
2026-01-02 20:45:13 +0530

    complete navbar structure, styling and implement dummy content home page

--- src/app/components/navbar/navbar.ts
+++ src/app/components/navbar/navbar.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from "@angular/router";
+import { Ripple } from "primeng/ripple";
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [RouterLink, Ripple, RouterLinkActive],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
 export class Navbar {
-
+  NAV_LINKS = [
+    {
+      label: 'Home',
+      path: '/home',
+      icon: "pi pi-home"
+    },
+    {
+      label: 'Users',
+      path: '/users',
+      icon: "pi pi-users"
+    },
+  ]
 }
--- src/app/pages/sign-in/sign-in.ts
+++ src/app/pages/sign-in/sign-in.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { Button } from 'primeng/button';
 import { CardModule } from 'primeng/card';
 import { FloatLabel } from 'primeng/floatlabel';
@@ -6,8 +8,25 @@ import { InputText } from 'primeng/inputtext';
 
 @Component({
   selector: 'app-sign-in',
-  imports: [CardModule, FloatLabel, InputText, Button],
+  imports: [CardModule, FloatLabel, InputText, Button, ReactiveFormsModule],
   templateUrl: './sign-in.html',
   styleUrl: './sign-in.css',
 })
-export class SignIn {}
+export class SignIn {
+  private fb = inject(FormBuilder);
+  private router = inject(Router);
+
+  signInForm: FormGroup = this.fb.group({
+    username: ['', Validators.required],
+    password: ['', Validators.required],
+  });
+
+  onSubmit() {
+    if (this.signInForm.valid) {
+      console.log('Form Submitted', this.signInForm.value);
+      this.router.navigate(['/home']);
+    } else {
+      this.signInForm.markAllAsTouched();
+    }
+  }
+}
